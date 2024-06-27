42e6dfb30633fdd25bd525c38d07f3fd7093ec7c
Darkbeast747474 <deepakkaushal7774@gmail.com>
2024-06-27 17:12:33 +0530

    up

--- src/MLProject/logger.py
+++ src/MLProject/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m-%d-%Y_%H-%M-%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+log_path=os.path.join(os.getcwd(),"logs")
 os.makedirs(log_path,exist_ok=True)
 
 LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
