4733f954ed56125fa8348f1cc584652eea1dd045
Darkbeast747474 <deepakkaushal7774@gmail.com>
2024-07-01 15:04:46 +0530

    Pred

--- src/MLProject/utils.py
+++ src/MLProject/utils.py
@@ -71,6 +71,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-
+    
     except Exception as e:
         raise CustomException(e, sys)      
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
